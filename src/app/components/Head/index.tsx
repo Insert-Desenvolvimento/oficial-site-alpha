a7c77e7d24797db3be66d863cda384b8cdac748b
Flavio Santos <flavio.pasantos@outlook.com>
2024-08-27 16:29:22 -0300

    feat: created component menu

---
+++ src/app/components/Head/index.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { Drawer, Button } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+import './header.scss';
+
+interface MenuItens {
+    name: string;
+    route: string;
+}
+
+interface MenuProps {
+    list: MenuItens[];
+}
+
+const Head = ({ list }: MenuProps) => {
+    const [visible, setVisible] = useState(false);
+
+    const showDrawer = () => {
+        setVisible(true);
+    };
+
+    const onClose = () => {
+        setVisible(false);
+    };
+
+    return (
+        <div className='container'>
+            <div className='content-head' onClick={() => window.alert("Você já está na página principal")}>
+                <img className='head-img' src='/logo.png' alt='Logo da Alpha Academia' />
+            </div>
+            <div className='gallery-menu'>
+                <ul className='content-gallery-menu'>
+                    {list?.map((item, index) => (
+                        <Link className={`menu-item ${item.route === "/register" ? "alpha" : item.route}`} href={item.route} key={index}>
+                            {item.name}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <Button
+                className='menu-button'
+                type='text'
+                icon={<MenuOutlined />}
+                onClick={showDrawer}
+            />
+            <Drawer
+                title='Menu'
+                placement='right'
+                onClose={onClose}
+                open={visible}
+                style={{ padding: 0 }}
+            >
+                <ul className='drawer-menu'>
+                    {list?.map((item, index) => (
+                        <li className={item.route === "/register" ? "drawer-alpha" : item.route} key={index} onClick={onClose} >
+                            <Link href={item.route}>{item.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </Drawer>
+        </div>
+    );
+};
+
+export default Head;
