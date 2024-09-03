ef62b578b2b7b31a7502c778c7f0539c12a30ea0
Flavio Santos <flavio.pasantos@outlook.com>
2024-09-03 14:17:20 -0300

    feat: adjusts and applyed icon in card modality

--- src/app/components/Modalitie/index.tsx
+++ src/app/components/Modalitie/index.tsx
@@ -1,6 +1,7 @@
 import { useModalityStore } from '@/app/store/modalitieDocs';
 import React, { useEffect } from 'react';
 import "./modalitie.scss"
+import { BicepsFlexed, Dumbbell, Play, Activity, Zap } from 'lucide-react';
 
 const Modalitie = () => {
     const { modalityDocs, fetchModalityDocs } = useModalityStore();
@@ -14,13 +15,28 @@ const Modalitie = () => {
     return (
         <div className='container-modalitie' id='home'>
             <div className='container-modality-info'>
-                <h1>Modalidades</h1>
+                <h1>Pensamos cada <span className='qualify-modality'>modalidade</span> com atenção para seu desenvolvimento.</h1>
                 <p>Temos diversas modalidades em nossa academia para que você tenha o melhor desenvolvimento do seu corpo e sua saúde</p>
             </div>
             <div className='container-list-modalities'>
-                <ul>
+                <div className='content-img'></div>
+                <ul className='list-mini-card-modality'>
                     {
-                        modalityDocs.map(item => (<li key={item.id}>{item.name}</li>))
+                        modalityDocs.map(item => (
+                            <li key={item.id} className='mini-card-modality'>
+
+                                {item.name}
+                                <span>
+                                    {item.name === "Luta" ? <BicepsFlexed size={"38px"} /> :
+                                        item.name === "Dança" ? <Play size={"38px"} /> :
+                                            item.name === "Musculação" ? <Dumbbell size={"38px"} /> :
+                                                item.name === "Funcional" ? <Activity size={"38px"} /> :
+                                                    <Zap size={"38px"} />}
+                                </span>
+
+                            </li>
+                        )
+                        )
                     }
                 </ul>
             </div>
