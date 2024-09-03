56ee09988ee5013b56d28cbb45e8bc0e4de6ff54
Flavio Santos <flavio.pasantos@outlook.com>
2024-09-03 16:25:22 -0300

    feat: applyed whats buttom

---
+++ src/app/components/Whatsapp/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./whatsapp.scss";
+
+const Whatsapp = () => {
+
+    return (
+        <div className="whats">
+            <a href='https://wa.me/553232761351?text=Olá! Venho pelo o Site e Tenho interesse em me matrícula. ' target='blank'>
+                <img src="/whatsapp.png" alt="Icone whatsapp para entrar em contato com a Alpha" title='Fale com a Alpha' />
+            </a>
+        </div>
+    );
+};
+
+export default Whatsapp;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import Personal from './components/Personal';
 import About from './components/About';
 import Products from './components/Products';
 import Combination from './components/CombinationProducts';
+import Whatsapp from './components/Whatsapp';
 const Home = () => {
   const list = [
     {
@@ -33,10 +34,6 @@ const Home = () => {
       route: "#about"
     },
     {
-      name: "Nossos Parceiros",
-      route: "#partners"
-    },
-    {
       name: "Contatos",
       route: "#contacts"
     },
@@ -53,19 +50,21 @@ const Home = () => {
   }, []);
 
   return (
-
-    <main className="container">
-      <div className='content-header'>
-        <Head list={list} />
-        <Banner />
-      </div>
-      <Modalitie />
-      <Space />
-      <Personal />
-      <Products />
-      <Combination />
-      <About />
-    </main>
+    <>
+      <main className="container">
+        <div className='content-header'>
+          <Head list={list} />
+          <Banner />
+        </div>
+        <Modalitie />
+        <Space />
+        <Personal />
+        <Products />
+        <Combination />
+        <About />
+      </main>
+      <Whatsapp />
+    </>
   );
 };
 
