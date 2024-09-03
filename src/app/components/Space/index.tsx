75b8cec469251a352a18a2367aad1145ffe9dc03
Flavio Santos <flavio.pasantos@outlook.com>
2024-09-03 10:08:26 -0300

    feat: applyed layout for space

--- src/app/components/Space/index.tsx
+++ src/app/components/Space/index.tsx
@@ -13,28 +13,25 @@ const Space = () => {
     return (
         <div className="container-space" id='space'>
             <div className="container-space-info">
-                <h1>Nossa Estrutura</h1>
+                <h1>Nosso Espaço é projetado para seu <span className='qualify-space'>conforto e praticidade.</span></h1>
                 <p>A Alpha oferece uma estrutura moderna, limpa e espaçosa para suas atividades</p>
             </div>
-            <Carousel
-                className="carousel-container"
-                dots
-                arrows
-                autoplay={true}
-            >
-                {spaceDocs.map((item) => (
-                    <Card
-                        key={item.id}
-                        className="space-card"
-                        cover={<img alt={item.name} src={item.imageUrl} className="space-image" />}
-                    >
-                        <Card.Meta
-                            title={item.name}
-                            description={item.description}
-                        />
-                    </Card>
-                ))}
-            </Carousel>
+            <div className='gallery-img-space'>
+                <ul className='gallery'>
+                    {
+                        spaceDocs.map((item, index) => (
+                            <li key={index} className='space-card'>
+                                <img alt={item.name} src={item.imageUrl} className="space-image" />
+                                <div className='text-card'>
+                                    <p className='space-text-name'>
+                                        {item.name}
+                                    </p>
+                                    <p>{item.description}</p>
+                                </div>
+                            </li>))
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
